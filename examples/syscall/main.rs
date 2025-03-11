68bc12f5078cc3c93594b43dca500732b1057484
joaoviictorti <joaovictorti08@gmail.com>
2025-03-10 21:28:21 -0300

    Adding new examples

---
+++ examples/syscall/main.rs
@@ -0,0 +1,21 @@
+use std::{ffi::c_void, ptr::null_mut};
+use dinvk::{
+    data::{HANDLE, NTSTATUS, NT_SUCCESS}, 
+    syscall, Dll
+};
+
+fn main() -> Result<(), NTSTATUS> {
+    // Alternatively, you can use Dll::Vertdll or Dll::Iumdll on x86_64
+    Dll::use_dll(Dll::Win32u);
+
+    // Memory allocation using a syscall
+    let mut addr = null_mut::<c_void>();
+    let mut size = (1 << 12) as usize;
+    let status = syscall!("NtAllocateVirtualMemory", -1isize as HANDLE, &mut addr, 0, &mut size, 0x3000, 0x04).ok_or(-1)?;
+    if !NT_SUCCESS(status) {
+        eprintln!("@ NtAllocateVirtualMemory Failed With Status: {}", status);
+        return Err(status);
+    }
+
+    Ok(())
+}
