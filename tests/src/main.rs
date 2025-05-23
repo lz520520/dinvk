fcb4bfdab87c8ac7bdf6a18c052af7277a8c905e
joaoviictorti <joaovictorti08@gmail.com>
2025-05-23 14:19:06 -0300

    Update tests

--- tests/src/main.rs
+++ tests/src/main.rs
@@ -1,10 +1,16 @@
-use dinvk::GetModuleHandle;
+use dinvk::{GetModuleHandle, GetProcAddress};
 
 #[test]
 fn test_modules() {
-    println!("{:?}", GetModuleHandle("kernel32.dll", None));
-    println!("{:?}", GetModuleHandle("kernel32.DLL", None));
-    println!("{:?}", GetModuleHandle("kernel32", None));
-    println!("{:?}", GetModuleHandle("KERNEL32.dll", None));
-    println!("{:?}", GetModuleHandle("KERNEL32", None));
+    println!("Module: {:?}", GetModuleHandle("kernel32.dll", None));
+    println!("Module: {:?}", GetModuleHandle("kernel32.DLL", None));
+    println!("Module: {:?}", GetModuleHandle("kernel32", None));
+    println!("Module: {:?}", GetModuleHandle("KERNEL32.dll", None));
+    println!("Module: {:?}", GetModuleHandle("KERNEL32", None));
+}
+
+#[test]
+fn test_function() {
+    let module = GetModuleHandle("KERNEL32.dll", None);
+    println!("Function: {:?}", GetProcAddress(module, "VirtualAlloc", None));
 }
